5f98406291f013949a110cfa942b256b889b588c
Gabriel Maurici <gabriel.maurici@hotmail.com>
2022-01-25 21:55:23 -0300

    Add projeto react

---
+++ ping-react/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dispositivos from './pages';
+
+function App() {
+  return (
+    <div className="App">
+      <Dispositivos/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ping-react/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dispositivos = (): JSX.Element => {
+    return(
+        <>
+            <h1>Hello</h1>
+        </>
+    )
+}
+
+export default Dispositivos;
